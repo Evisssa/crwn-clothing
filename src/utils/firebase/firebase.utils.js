92a1c922901cc6d3b64514a2ecb6fa9e6280fdaf
Evisssa <evisa.skarra@gmail.com>
2024-02-11 00:23:38 +0100

    adding firebase util

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCeCUrRf9lf9irvqJVuWmnMHVtFIWemozM",
+    authDomain: "crwn-clothing-db-81acc.firebaseapp.com",
+    projectId: "crwn-clothing-db-81acc",
+    storageBucket: "crwn-clothing-db-81acc.appspot.com",
+    messagingSenderId: "1029933655345",
+    appId: "1:1029933655345:web:4066967dd1c0e05528fc10"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
